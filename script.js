d1c042fb938d2de4a4bfef2756b0c80383219631
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-01-18 17:08:52 -0800

    basic checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your JS goes here
+
+for (let i = 1; i < 82; i++) {
+  let redValue = Math.floor(Math.random() * (255 + 1));
+  let greenValue = Math.floor(Math.random() * (255 + 1));
+  let blueValue = Math.floor(Math.random() * (255 + 1));
+
+  let tile = document.createElement('div')
+  tile.innerHTML = "<div style='display:block; padding-bottom:11.1%;float:left;width:11.1%'>&nbsp;</div>"
+  document.body.append(tile)
+
+}
